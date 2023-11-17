c4800d14568bf374859a4f205766dc4f36f3c581
현진 빙 <bbs234@hotmail.com>
2023-11-17 14:42:32 +0900

    feat : Done fizzbuzz

--- fizzbbuzz.py
+++ fizzbbuzz.py
@@ -1,7 +1,11 @@
 print("test")
 
 for i in range(1, 16 + 1):
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print("izzbuzz")
+    elif i % 3 == 0:
         print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
     else:
         print(i)
